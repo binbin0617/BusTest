eb6860bd1ac90c4c428fae9d20b4605915bc95d3
dongbinbin <binbin5211314>
2020-03-31 17:47:07 +0800

    baidu

--- app/src/main/java/com/bin/bustest/aty/MainAty.java
+++ app/src/main/java/com/bin/bustest/aty/MainAty.java
@@ -1,19 +1,33 @@
 package com.bin.bustest.aty;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.bin.bustest.R;
+import com.bin.bustest.bean.LocationBean;
 import com.bin.bustest.fgt.FirstFragment;
 import com.bin.bustest.fgt.SecondFragment;
 import com.next.easynavigation.view.EasyNavigationBar;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainAty extends AppCompatActivity {
+
+    private String TAG = MainAty.class.getSimpleName();
     private String[] tabText = {"首页", "发现", "消息", "我的"};
     //未选中icon
     private int[] normalIcon = {R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
@@ -23,15 +37,31 @@ public class MainAty extends AppCompatActivity {
     private List<Fragment> fragments = new ArrayList<>();
 
     private EasyNavigationBar navigationBar;
+
+
+    public LocationClient mLocationClient = null;
+    private MyLocationListener myListener = new MyLocationListener();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationClient = new LocationClient(getApplicationContext());
+        //声明LocationClient类
+        mLocationClient.registerLocationListener(myListener);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                }, 00000000);
+            }
+        }
         initView();
+        initLocationOption();
     }
 
-    public void initView(){
+    public void initView() {
 
         navigationBar = findViewById(R.id.navigationBar);
         fragments.add(new FirstFragment());
@@ -46,4 +76,101 @@ public class MainAty extends AppCompatActivity {
                 .fragmentManager(getSupportFragmentManager())
                 .build();
     }
+
+    /**
+     * 初始化定位参数配置
+     */
+
+    private void initLocationOption() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，设置定位模式，默认高精度
+        //LocationMode.Hight_Accuracy：高精度；
+        //LocationMode. Battery_Saving：低功耗；
+        //LocationMode. Device_Sensors：仅使用设备；
+        option.setCoorType("bd09ll");
+        //可选，设置返回经纬度坐标类型，默认GCJ02
+        //GCJ02：国测局坐标；
+        //BD09ll：百度经纬度坐标；
+        //BD09：百度墨卡托坐标；
+        //海外地区定位，无需设置坐标类型，统一返回WGS84类型坐标
+        option.setScanSpan(0);
+        //可选，设置发起定位请求的间隔，int类型，单位ms
+        //如果设置为0，则代表单次定位，即仅定位一次，默认为0
+        //如果设置非0，需设置1000ms以上才有效
+        option.setOpenGps(true);
+        //可选，设置是否使用gps，默认false
+        //使用高精度和仅用设备两种定位模式的，参数必须设置为true
+        option.setLocationNotify(false);
+        //可选，设置是否当GPS有效时按照1S/1次频率输出GPS结果，默认false
+        option.setIgnoreKillProcess(false);
+        //可选，定位SDK内部是一个service，并放到了独立进程。
+        //设置是否在stop的时候杀死这个进程，默认（建议）不杀死，即setIgnoreKillProcess(true)
+        option.SetIgnoreCacheException(false);
+        //可选，设置是否收集Crash信息，默认收集，即参数为false
+        option.setWifiCacheTimeOut(5 * 60 * 1000);
+        //可选，V7.2版本新增能力
+        //如果设置了该接口，首次启动定位时，会先判断当前Wi-Fi是否超出有效期，若超出有效期，会先重新扫描Wi-Fi，然后定位
+        option.setEnableSimulateGps(false);
+        //可选，设置是否需要过滤GPS仿真结果，默认需要，即参数为false
+        option.setNeedNewVersionRgc(true);
+        //可选，设置是否需要最新版本的地址信息。默认不需要，即参数为false
+        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
+        //更多LocationClientOption的配置，请参照类参考中LocationClientOption类的详细说明
+        option.setIsNeedLocationPoiList(true);
+        //可选，是否需要周边POI信息，默认为不需要，即参数为false
+        //如果开发者需要获得周边POI信息，此处必须为true
+        mLocationClient.setLocOption(option);
+        //开始定位
+        mLocationClient.start();
+    }
+
+    /**
+     * 实现定位回调
+     */
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+//            //此处的BDLocation为定位结果信息类，通过它的各种get方法可获取定位相关的全部结果
+//            //以下只列举部分获取经纬度相关（常用）的结果信息
+//            //更多结果信息获取说明，请参照类参考中BDLocation类中的说明
+//
+            //获取纬度信息
+            double latitude = location.getLatitude();
+            //获取经度信息
+            double longitude = location.getLongitude();
+
+            Log.e(TAG, "-->" + latitude + longitude);
+            LocationBean locationBean = new LocationBean();
+            locationBean.setLatitude(latitude);
+            locationBean.setLongitude(longitude);
+
+
+            EventBus.getDefault().post(locationBean);
+
+//            initView(latitude, longitude);
+//            //获取定位精度，默认值为0.0f
+//            float radius = location.getRadius();
+//            //获取经纬度坐标类型，以LocationClientOption中设置过的坐标类型为准
+//            String coorType = location.getCoorType();
+//            //获取定位类型、定位错误返回码，具体信息可参照类参考中BDLocation类中的说明
+//            int errorCode = location.getLocType();
+//            Log.e(TAG, "-->" + latitude + longitude + radius + coorType + errorCode);
+//            Log.e(TAG, "-->111");
+//
+////            PoiNearbySearchOption
+//            Poi poi = location.getPoiList().get(0);
+//            String poiName = poi.getName();    //获取POI名称
+//            String poiTags = poi.getTags();    //获取POI类型
+//            String poiAddr = poi.getAddr();    //获取POI地址 //获取周边POI信息
+//            Log.e(TAG, "-->" + poiName + poiTags + poiAddr);
+//
+//            PoiRegion poiRegion = location.getPoiRegion();
+//            String poiDerectionDesc = poiRegion.getDerectionDesc();    //获取PoiRegion位置关系
+//            String poiRegionName = poiRegion.getName();    //获取PoiRegion名称
+//            String poiTags1 = poiRegion.getTags();    //获取PoiRegion类型
+//            Log.e(TAG, "-->" + poiDerectionDesc + poiRegionName + poiTags1);
+        }
+    }
+
 }
--- app/src/main/java/com/bin/bustest/fgt/SecondFragment.java
+++ app/src/main/java/com/bin/bustest/fgt/SecondFragment.java
@@ -1,7 +1,155 @@
 package com.bin.bustest.fgt;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
-public class SecondFragment extends Fragment {
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.busline.BusLineResult;
+import com.baidu.mapapi.search.busline.BusLineSearch;
+import com.baidu.mapapi.search.busline.BusLineSearchOption;
+import com.baidu.mapapi.search.busline.OnGetBusLineSearchResultListener;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
+import com.baidu.mapapi.search.poi.PoiSortType;
+import com.bin.bustest.R;
+import com.bin.bustest.aty.MainAty;
+import com.bin.bustest.bean.LocationBean;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.List;
+
+public class SecondFragment extends Fragment implements OnGetBusLineSearchResultListener {
+
+    private String TAG = SecondFragment.class.getSimpleName();
+
+    private PoiSearch mPoiSearch;
+
+    private BusLineSearch mBusLineSearch;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fgt_first, container, false);
+        EventBus.getDefault().register(this);
+        return view;
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void Event(LocationBean locationBean) {
+        Log.e(TAG, "-->" + locationBean.getLatitude() + "aaaaaaa");
+        initView(locationBean);
+    }
+
+    private void initView(LocationBean locationBean) {
+
+        mPoiSearch = PoiSearch.newInstance();
+
+//        mBusLineSearch = BusLineSearch.newInstance();
+//
+//        mBusLineSearch.setOnGetBusLineSearchResultListener(this);
+
+        mPoiSearch.setOnGetPoiSearchResultListener(onGetPoiSearchResultListener);
+
+        mPoiSearch.searchInCity((new PoiCitySearchOption())
+                .city("天津")
+                .keyword("ktv")
+                .pageNum(10));
+//        PoiNearbySearchOption option = new PoiNearbySearchOption();
+//        option.keyword("华苑");
+//        option.sortType(PoiSortType.distance_from_near_to_far);
+//        option.location(new LatLng(locationBean.getLatitude(), locationBean.getLongitude()));
+////        if (radius != 0) {
+////            option.radius(radius);
+////        } else {
+//        option.radius(1000);
+////        }
+//
+//        option.pageCapacity(20);
+//        mPoiSearch.searchNearby(option);
+    }
+
+
+    //周边
+    private OnGetPoiSearchResultListener onGetPoiSearchResultListener = new OnGetPoiSearchResultListener() {
+        @Override
+        public void onGetPoiResult(PoiResult poiResult) {
+            if (poiResult == null || poiResult.error != SearchResult.ERRORNO.NO_ERROR) {
+                Toast.makeText(getContext(), "OnGetPoiSearchResultListener抱歉，未找到结果", Toast.LENGTH_LONG).show();
+                return;
+            }
+            // 遍历所有poi，找到类型为公交线路的poi
+            for (PoiInfo poi : poiResult.getAllPoi()) {
+                if (poi.type == PoiInfo.POITYPE.BUS_LINE
+                        || poi.type == PoiInfo.POITYPE.SUBWAY_LINE) {
+                    //如下代码为发起检索代码，定义监听者和设置监听器的方法与POI中的类似
+                    mBusLineSearch.searchBusLine((new BusLineSearchOption()
+                            //我这里的城市写死了，和我要查的是一样的
+                            .city("天津")
+                            .uid(poi.uid)));
+                }
+            }
+        }
+
+        @Override
+        public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
+        }
+
+        @Override
+        public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
+
+        }
+
+        @Override
+        public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
+        }
+    };
+
+    @Override
+    public void onGetBusLineResult(BusLineResult busLineResult) {
+        if (busLineResult == null || busLineResult.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(getContext(), "抱歉，未找到结果", Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(getContext(), busLineResult.getBusLineName(),
+                Toast.LENGTH_SHORT).show();
+        Log.e("MainActivity-->", busLineResult.toString());
+        List<BusLineResult.BusStation> steps = busLineResult.getStations();
+        StringBuffer sb = new StringBuffer();
+        for (BusLineResult.BusStation b : steps) {
+            sb.append("-->");
+            sb.append(b.getTitle());
+        }
+        Log.e("MainActivity-->", sb.toString());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+    }
+
 
 }
